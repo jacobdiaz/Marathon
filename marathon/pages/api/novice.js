1a684a63f51b67296680927edbae70592d29691c
Jacob Diaz <hello@jacobdiaz.dev>
2022-01-02 18:26:49 -0600

    doc: Created novice-1 schedule

---
+++ marathon/pages/api/novice.js
@@ -0,0 +1,261 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Novice - 1 : https://www.halhigdon.com/training-programs/marathon-training/novice-1-marathon/
+const novice = {
+  marathon_date: "10/09/2022",
+  week1: {
+    week_number: 1,
+    month_number: 1,
+    total_miles: 15,
+    days: {
+      d1: "rest", // mon
+      d2: "3", // tue
+      d3: "3", // wed
+      d4: "3", // thu
+      d5: "rest", // fri
+      d6: "6", // sat
+      d7: "cross", // sun
+    },
+  },
+  week2: {
+    week_number: 2,
+    month_number: 1,
+    total_miles: 16,
+    days: {
+      d1: "rest", // mon
+      d2: "3", // tue
+      d3: "3", // wed
+      d4: "3", // thu
+      d5: "rest", // fri
+      d6: "7", // sat
+      d7: "cross", // sun
+    },
+  },
+  week3: {
+    week_number: 3,
+    month_number: 1,
+    total_miles: 15,
+    days: {
+      d1: "rest", // mon
+      d2: "3", // tue
+      d3: "4", // wed
+      d4: "3", // thu
+      d5: "rest", // fri
+      d6: "5", // sat
+      d7: "cross", // sun
+    },
+  },
+  week4: {
+    week_number: 4,
+    month_number: 1,
+    total_miles: 19,
+    days: {
+      d1: "rest", // mon
+      d2: "3", // tue
+      d3: "4", // wed
+      d4: "3", // thu
+      d5: "rest", // fri
+      d6: "9", // sat
+      d7: "cross", // sun
+    },
+  },
+  week5: {
+    week_number: 5,
+    month_number: 2,
+    total_miles: 21,
+    days: {
+      d1: "rest", // mon
+      d2: "3", // tue
+      d3: "5", // wed
+      d4: "3", // thu
+      d5: "rest", // fri
+      d6: "10", // sat
+      d7: "cross", // sun
+    },
+    week6: {
+      week_number: 6,
+      month_number: 2,
+      total_miles: 18,
+      days: {
+        d1: "rest", // mon
+        d2: "3", // tue
+        d3: "5", // wed
+        d4: "3", // thu
+        d5: "rest", // fri
+        d6: "7", // sat
+        d7: "cross", // sun
+      },
+    },
+    week7: {
+      week_number: 7,
+      month_number: 2,
+      total_miles: 24,
+      days: {
+        d1: "rest", // mon
+        d2: "3", // tue
+        d3: "6", // wed
+        d4: "3", // thu
+        d5: "rest", // fri
+        d6: "12", // sat
+        d7: "cross", // sun
+      },
+    },
+    week8: {
+      week_number: 8,
+      month_number: 2,
+      total_miles: 25.1,
+      days: {
+        d1: "rest", // mon
+        d2: "3", // tue
+        d3: "6", // wed
+        d4: "3", // thu
+        d5: "rest", // fri
+        d6: "rest", // sat
+        d7: "13.1", // sun
+      },
+    },
+    week9: {
+      week_number: 9,
+      month_number: 3,
+      total_miles: 24,
+      days: {
+        d1: "rest", // mon
+        d2: "3", // tue
+        d3: "7", // wed
+        d4: "4", // thu
+        d5: "rest", // fri
+        d6: "10", // sat
+        d7: "rest", // sun
+      },
+    },
+    week10: {
+      week_number: 10,
+      month_number: 3,
+      total_miles: 29,
+      days: {
+        d1: "rest", // mon
+        d2: "3", // tue
+        d3: "7", // wed
+        d4: "4", // thu
+        d5: "rest", // fri
+        d6: "15", // sat
+        d7: "rest", // sun
+      },
+    },
+    week11: {
+      week_number: 11,
+      month_number: 3,
+      total_miles: 32,
+      days: {
+        d1: "rest", // mon
+        d2: "4", // tue
+        d3: "8", // wed
+        d4: "4", // thu
+        d5: "rest", // fri
+        d6: "16", // sat
+        d7: "rest", // sun
+      },
+    },
+    week12: {
+      week_number: 12,
+      month_number: 3,
+      total_miles: 29,
+      days: {
+        d1: "rest", // mon
+        d2: "4", // tue
+        d3: "8", // wed
+        d4: "5", // thu
+        d5: "rest", // fri
+        d6: "12", // sat
+        d7: "rest", // sun
+      },
+    },
+    week13: {
+      week_number: 13,
+      month_number: 4,
+      total_miles: 36,
+      days: {
+        d1: "rest", // mon
+        d2: "4", // tue
+        d3: "9", // wed
+        d4: "5", // thu
+        d5: "rest", // fri
+        d6: "18", // sat
+        d7: "rest", // sun
+      },
+    },
+    week14: {
+      week_number: 14,
+      month_number: 4,
+      total_miles: 33,
+      days: {
+        d1: "rest", // mon
+        d2: "5", // tue
+        d3: "9", // wed
+        d4: "5", // thu
+        d5: "rest", // fri
+        d6: "14", // sat
+        d7: "rest", // sun
+      },
+    },
+    week15: {
+      week_number: 15,
+      month_number: 4,
+      total_miles: 40,
+      days: {
+        d1: "rest", // mon
+        d2: "5", // tue
+        d3: "10", // wed
+        d4: "5", // thu
+        d5: "rest", // fri
+        d6: "20", // sat
+        d7: "rest", // sun
+      },
+    },
+    week16: {
+      week_number: 16,
+      month_number: 4,
+      total_miles: 29,
+      days: {
+        d1: "rest", // mon
+        d2: "5", // tue
+        d3: "8", // wed
+        d4: "4", // thu
+        d5: "rest", // fri
+        d6: "12", // sat
+        d7: "rest", // sun
+      },
+    },
+    week17: {
+      week_number: 17,
+      month_number: 5,
+      total_miles: 21,
+      days: {
+        d1: "rest", // mon
+        d2: "4", // tue
+        d3: "6", // wed
+        d4: "3", // thu
+        d5: "rest", // fri
+        d6: "8", // sat
+        d7: "rest", // sun
+      },
+    },
+    week18: {
+      week_number: 18,
+      month_number: 5,
+      total_miles: 9,
+      days: {
+        d1: "rest", // mon
+        d2: "3", // tue
+        d3: "4", // wed
+        d4: "2", // thu
+        d5: "rest", // fri
+        d6: "rest", // sat
+        d7: "marathon", // sun
+      },
+    },
+  },
+};
+
+export default function handler(req, res) {
+  res.status(200).json(novice);
+}
