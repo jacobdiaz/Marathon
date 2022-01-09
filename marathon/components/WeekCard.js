26006396e2a7fe1563a300e8d5390311dea75e8d
Jacob Diaz <hello@jacobdiaz.dev>
2022-01-09 13:56:37 -0600

    feat: Aligned Weekday Item and installed materialUI package

--- marathon/components/WeekCard.js
+++ marathon/components/WeekCard.js
@@ -4,7 +4,6 @@ import * as stats from "../lib/stats"; // Todo to use individual elements
 
 export default function WeekCard({ schedule }) {
   const currentWeekData = schedule[stats.currentTrainingWeek - 1];
-  console.log(currentWeekData);
   const renderDays = () => {
     let days = [];
     for (let i = 1; i <= 7; i++) {
@@ -12,7 +11,7 @@ export default function WeekCard({ schedule }) {
         <WeekDayItem
           key={i}
           day={i}
-          distance={`${currentWeekData.days[`d${i}`]} Miles`}
+          distance={currentWeekData.days[`d${i}`]}
           isToday={i === 0}
           // distance={schedule[stats.currentTrainingWeek - 1].days[i].distance}
         />
@@ -23,7 +22,7 @@ export default function WeekCard({ schedule }) {
 
   return (
     <div>
-      <h2>Week</h2>
+      <h2>Week {`${stats.currentTrainingWeek}`}</h2>
       <div className="w-full p-6 bg-white rounded">
         <h1>This Week</h1>
         {renderDays()}
--- marathon/components/WeekDayItem.js
+++ marathon/components/WeekDayItem.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 export default function WeekDayItem({ day, isToday, distance }) {
-    
-    return (
-    <div>
-      <p>{day}</p>
-      {isToday && <p>Today</p>}
-      <p>{distance}</p>
+  const weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  return (
+    <div className="my-2 py-4 flex w-full justify-between bg-slate-300 ">
+      {/* day = 1-7 */}
+      <div className=" bg-green-300">
+        <p>{weekday[day - 1]}</p>
+      </div>
+      <div className=" bg-red-400">
+        <p className="">{distance === "rest" ? distance : distance + " Miles"}</p>
+      </div>
     </div>
   );
 }
